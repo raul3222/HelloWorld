7cd677c1ba2c186ddd7c9ae9667387b01ba6f592
R.Shafigin <sh_raul08@mail.ru>
2021-10-18 22:10:16 +0200

    Algorithm creation

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var helloWorldLabel: UILabel!
+    
+    @IBOutlet var showTextButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        helloWorldLabel.isHidden = true
+        showTextButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func showTextButtonPressed() {
+        helloWorldLabel.isHidden.toggle()
+        
+        if helloWorldLabel.isHidden {
+            showTextButton.setTitle("Show text", for: .normal)
+        } else {
+            showTextButton.setTitle("Hide text", for: .normal)
+        }
+    }
+    
+ 
 }
 
